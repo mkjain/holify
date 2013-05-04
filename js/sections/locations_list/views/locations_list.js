c4dd76272aec8a7f4953561898d0671a450d9587
Tobias Otte <tobias.otte@xing.com>
2013-05-04 20:10:36 +0200

    rendering is working now

--- js/sections/locations_list/views/locations_list.js
+++ js/sections/locations_list/views/locations_list.js
@@ -2,15 +2,11 @@ app.sections.locationsList.views.LocationsList = Backbone.View.extend({
 
   el: "#trip-list",
 
-  template: app.templates.locationsList,
-
   initialize: function() {
-    // console.log(app.templates.locationsList);
   },
 
   render: function() {
-    console.log(this);
-    //this.$el.html(this.template(this.model.toJSON()));
+    this.$el.html(app.templates.locationsList(this.model.toJSON()));
   }
 
 });
