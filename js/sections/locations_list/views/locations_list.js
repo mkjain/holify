621ef100398efdd4002efcfb08d10889a8a95e87
Tobias Otte <tobias.otte@xing.com>
2013-05-05 07:39:33 +0200

    add remove styling

--- js/sections/locations_list/views/locations_list.js
+++ js/sections/locations_list/views/locations_list.js
@@ -2,6 +2,10 @@ app.sections.locationsList.views.LocationsList = Backbone.View.extend({
 
   el: "#trip-list #list",
 
+  events: {
+    "click li .remove": "onClick"
+  },
+
   initialize: function() {
     _.bindAll(this, "render");
     this.collection.on("all", this.render);
@@ -9,6 +13,14 @@ app.sections.locationsList.views.LocationsList = Backbone.View.extend({
 
   render: function() {
     this.$el.html(app.templates.locationsList(this.collection.toJSON()));
+  },
+
+  onClick: function(event) {
+    var id = $(event.currentTarget).parent().data("id"),
+        model = this.collection.get(id);
+
+
+    model.destroy();
   }
 
 });
